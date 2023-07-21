00d0df092a06bc420b15cd7ce904a20a7a6f24ff
djsnipa1 <chadboyce@gmail.com>
2023-07-21 05:15:29 +0000

    Hopefully updates on replit

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,19 @@
   import Stars from "./Stars.svelte";
   import Title from "./Title.svelte";
   import Swipe from "./Swipe.svelte";
+    import Congrats from "./Congrats.svelte"
 </script>
 
+<svelte:head>
+    <!-- umami tracking code -->
+    <script async src="https://umami-djsnipa1.vercel.app/script.js" data-website-id="4be6d0fe-61c3-4ef7-b905-db70af8448b0"></script>
+</svelte:head>
 
-
+<Title />
+<Congrats />
 <Stars />
 <Swipe />
-<Title />
+<!-- <Title /> -->
 
 
 
